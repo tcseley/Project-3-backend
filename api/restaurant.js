83ff1d0b9975c5a9d5b9194bc84f3b340af771cc
janeth <ledezmajane@berkeley.edu>
2021-06-10 10:03:44 -0700

    add restaurant router

--- api/restaurant.js
+++ api/restaurant.js
@@ -3,4 +3,26 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-e
+
+//models
+const { Restaurant } = require('../models');
+
+
+//controllers
+const index = async (req, res) => {
+    console.log('inside of /api/restaurant');
+    try {
+        const allRestaurants = await Restaurant.find({});
+
+        res.json({ restaurants: allRestaurants });
+    } catch (error) {
+        console.log('Error inside of /api/restaurant');
+        console.log(error);
+        return res.status(400).json({ message: 'Restaurants not found... Please try again.' });
+    }
+}
+
+// GET -> /api/restaurants/
+router.get('/', index); 
+
+module.exports = router;

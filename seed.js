c23b2f69b2d7ecb86c2580539aca950242d54967
tcseley <tiborcseley@gmail.com>
2021-06-10 10:18:25 -0500

    seed.js review

--- seed.js
+++ seed.js
@@ -37,22 +37,84 @@
 //     console.log(results);
 // });
 
-const mongoose = require('mongoose');
+//-----------------------------------------------------------
 
-const blogSchema = new mongoose.Schema({
-    restaurants: [{type: mongoose.Schema.Types.ObjectId, ref: 'Restaurant'}]
-});
+const { Restaurant, User, Blog } = require('./models');
+Restaurant.create([
+        {
+            name: 'Restaurant 1',
+            location: 'Austin, TX',
+            review: '* * *',
+            price: '$',
+            phone: 5123458989,
+        },
+        {
+            name: 'Restaurant 2',
+            location: 'Austin, TX',
+            review: '* *',
+            price: '$$$',
+            phone: 5122346789,
+        },
+        {
+            name: 'Restaurant 3',
+            location: 'Austin, TX',
+            review: '*',
+            price: '$',
+            phone: 512087765,
+        },
+        {
+            name: 'Restaurant 4',
+            location: 'Austin, TX',
+            review: '* * * *',
+            price: '$$$$',
+            phone: 5121239274,
+        }
+    ], (err, results) => {
+        console.log(results);
+    });
 
-const Blog = mongoose.model('Blog', blogSchema);
-
-const restaurantSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-});
-
-const Restaurant = mongoose.model('Restaurant', restaurantSchema);
 
 
 
+
+
+
+// //create a test user
+// const createUser = async (obj) => {
+//     User.create(obj, (err, user) => {
+//         if(err) console.log(err);
+//         console.log(user);
+//     })
+// }
+// const Janeth = {
+//     name: 'Janeth',
+//     email: 'Janeth@gmail.com',
+//     password: '123456789'
+// }
+// // createUser(Janeth);
+// //Find user and then push restaurant to user
+// User.findById('60c15774b527025960ef0238', (err, user) => {
+//     console.log(user);
+//     //find Restaurant 
+// Restaurant.findOne({ _id: '60c1645b8be3345ecdd75f99'}, (err, restaurant) => {
+//     console.log(restaurant);
+//     user.restaurant.push({restaurant});
+//     User.save();
+//     console.log(User)
+// })
+// })
+// // 
+// const fetchOneUser = async (email) => {
+//     const result = await User.findOne({ email })
+//     console.log(result);
+// }
+// // fetchOneUser('Janeth@gmail.com')
+// //remove restaurant by id
+// const removeOne = async(_id) => {
+//     Restaurant.findByIdAndRemove({_id}, (err) =>{
+//         if (err) console.log(err);
+//         console.log('Restaurant was delted!')
+//     })
+// }
 
 

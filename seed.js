ee596442c676c8c310f68a9884a94aef82eb3d98
tcseley <tiborcseley@gmail.com>
2021-06-15 11:24:49 -0500

    seed file

--- seed.js
+++ seed.js
@@ -81,40 +81,20 @@
 
 
 // import all models
-const { Book } = require('./models');
-Book.create([
+const { User } = require('./models');
+User.create([
     {
-        title: 'Book 1',
-        author: 'Author 1',
-        pages: 199,
-        genre: 'Business',
-        price: 20,
-        isbn: '902391340123941203192'
+        name: 'Joe',
+        email: 'joe@test.com',
+        password: 'password1!',
+        
     },
     {
-        title: 'Book 2',
-        author: 'Author 2',
-        pages: 300,
-        genre: 'Software',
-        price: 100,
-        isbn: '9023913455523941203192'
+        name: 'Jane',
+        email: 'jane@test.com',
+        password: 'password2!',
     },
-    {
-        title: 'Book 3',
-        author: 'Author 3',
-        pages: 199,
-        genre: 'Business',
-        price: 20,
-        isbn: '9023913403343441203192'
-    },
-    {
-        title: 'Book 4',
-        author: 'Author 4',
-        pages: 333,
-        genre: 'Software',
-        price: 20,
-        isbn: '9023912123941203192'
-    }
+    
 ], (err, results) => {
     console.log(results);
 });
